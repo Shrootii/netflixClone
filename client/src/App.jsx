48a54e14c99df98e0f1d6a5745e0142bda9af324
TSERING WANGZES <2020csb1136@iitrpr.ac.in>
2023-04-12 23:25:18 +0530

    tsering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,21 @@ import Home from './pages/home/Home'
 import Watch from './pages/watch/Watch'
 import Register from './pages/register/Register'
 import Login from './pages/login/Login'
+import { Routes, Route } from "react-router-dom"
 const App = () => {
-  return <Watch/>;
+  return (
+<>
+<Routes>
+
+<Route path='/' element={<Login />} />
+<Route path='/register' element={<Register/>} />
+<Route path='/watch' element={<Watch/>} />
+<Route path='/home' element={<Home/>} />
+
+</Routes>
+
+</>
+  );
 };
 
 export default App;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../../components/navbar/Navbar"
 import List from "../../components/list/List"
+import Featured from "../../components/featured/Featured"
 
 import "./home.scss"
 import React from 'react'
@@ -8,6 +9,7 @@ function Home() {
   return (
     <div className="home">
       <Navbar/> 
+      {/* <Featured/> */}
       <img width ="100%" src="https://pbs.twimg.com/media/C1N_8R2WEAABYWK.jpg" alt="" />
       <List/>
       <List/>
