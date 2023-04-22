e6de4057f70946a74bb14bb2e44e3c65d7937aab
ayushiP27 <93206036+ayushiP27@users.noreply.github.com>
2023-04-22 14:28:47 +0530

    Update Listitem.jsx

--- client/src/components/listitem/Listitem.jsx
+++ client/src/components/listitem/Listitem.jsx
@@ -10,7 +10,7 @@ import ThumbDownOffAltOutlinedIcon from '@mui/icons-material/ThumbDownOffAltOutl
 
 import { useState } from "react";
 
-export default function Listitem() {
+export default function Listitem(index) {
   const [isHovered, setIsHovered]=useState(false);
   const trailer = "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
   
@@ -32,10 +32,10 @@ export default function Listitem() {
              <video src = {trailer} autoPlay={true} loop />
              <div className="itemInfo">
               <div className="icons">
-               <PlayArrow/>
-               <Add/>
-               <ThumbUpAltOutlined/>
-               <ThumbDownOutlined/>
+               <PlayCircleOutlineOutlinedIcon/>
+               <AddCircleOutlineOutlinedIcon/>
+               <ThumbUpAltOutlinedIcon/>
+               <ThumbDownOffAltOutlinedIcon/>
               </div>
             <div className="itemInfoTop">
              <span>1 hour 20 minutes</span>
@@ -54,3 +54,5 @@ export default function Listitem() {
 </div>
 );
 }
+
+
